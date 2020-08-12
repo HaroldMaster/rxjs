da31fb7f9cdf145ee461a949d66299057dc8dc84
Harold Flores <bhflores1994@gmail.com>
2020-08-11 20:59:13 -0500

    se añade interval y timer

--- src/app/components/intervalytimer/intervalytimer.component.ts
+++ src/app/components/intervalytimer/intervalytimer.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, timer, Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-intervalytimer',
   templateUrl: './intervalytimer.component.html',
-  styleUrls: ['./intervalytimer.component.scss']
+  styleUrls: ['./intervalytimer.component.scss'],
 })
 export class IntervalytimerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public contador: any;
+  public tiempo: any;
+  public aux: boolean;
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  constructor() {
+    this.aux = true;
+    //this.tiempo = timer(12000);
+    this.contador = interval(1000).pipe(takeUntil(this.contador = timer(12000)));
   }
 
+  ngOnInit(): void {}
+
+  interval() {
+    this.aux = false;
+    this.contador.subscribe((n) => {
+      let resultado = document.querySelector("#resultado");
+      let msg1 = document.createElement("p");
+      msg1.innerHTML=n;
+      resultado.appendChild(msg1);
+      console.log(n);
+      if (n == 10) {
+        msg1.innerHTML="Se acabo";
+      }
+    });
+  }
 }
